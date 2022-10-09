5b72736dacecdb87b36649fdd9ede612b47e894c
Yanuario Bagas Prayoga <contact@kakandazyan.my.id>
2022-10-10 02:58:24 +0700

    Add files via upload

---
+++ pembagian.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.Math;
+
+public class pembagian {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int bil1, bil2, hasil;
+        
+        bil1 = input.nextInt();
+        bil2 = input.nextInt();
+        hasil = bil1 / bil2;
+        System.out.println(hasil);
+    }
+}
